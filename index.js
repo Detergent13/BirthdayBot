a2a0c1b3eb0dfa7ed7ea558691fe168ba73dd2f1
Detergent13 <50428606+Detergent13@users.noreply.github.com>
2021-01-22 13:57:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const Schedule = require('node-schedule');
+const schedule = require('node-schedule');
 const fs = require('fs');
 const client = new Discord.Client();
 const fileName = './birthdays.json';
@@ -8,17 +8,22 @@ client.login(process.env.BOT_TOKEN)
     .then(main, main)
     .catch(console.error);
 
-// load up json? birthdays upon launch from file, create events for each of them
+// load up json(?) birthdays upon launch from file, create events for each of them here
 
-function announceBirthday(user) {
-    // send message in channel (maybe create a new channel w/ perms?) saying that it's @user's birthday here
+// example of a new birthday event below - December 13th, each year at noon (wildcard is to allow it on any day of the week)
+// probably need to move this into the b!birthday command + startup loop eventually
 
+var j = schedule.scheduleJob(new Date('0 12 13 12 *', function(){
+  console.log('announced <user>\'s birthday');
+  // send message in channel (maybe create a new channel w/ perms excluding the person?) saying that it's @user's birthday here
+});
 
 function main() {
     client.on('ready', () => {
         console.log(`Logged in as $(client.user.tag)!`);
     })
 
+    // not working for whatever reason, god knows why - check if this is deprecated now maybe
     client.on('ready', () => {
         client.user.setActivity('for your birthday :\)', {type: 'WATCHING'})
     })
@@ -30,12 +35,13 @@ function main() {
             return;
         else if (msg.content.startsWith('b!birthday'))
             arg1 = msg.content.split(' ')[1];
-            // regex magic to let us check if the arg matches mm/dd/yyyy roughly
+            // regex magic to let us check if the arg matches mm/dd/yyyy roughly (possibly not necessary anymore with cron format/Date class- check if it verifies.
             if(arg1.match(/\d{1,2}\/\d{1,2}\/\d{4}/g)) {
                 datesplit = arg1.split('/');
                 if (parseInt(datesplit[0]) > 12 || parseInt(datesplit[1]) > 31 || (parseInt(datesplit[2]) > parseInt(new Date().getFullYear())))
                     msg.channel.send('Something is wrong with the format of your date. Please try again! (bounds)');
                 else {
+                    // I have absolutely zero clue how to properly store JSONs, we'll see how I can sort this out
                     fs.readFile(fileName, function (err, data) {
                         let json = JSON.parse(data);
                         json.push(`\"$(msg.author.id)\": \"arg1\"`);
